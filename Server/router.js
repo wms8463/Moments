2e121317b31b53e972abfffd2d03f2467b090d65
Will Sukonik <will.sukonik@icloud.com>
2021-09-22 15:00:30 +0200

    router get and post created, koa dependencies installed

--- Server/router.js
+++ Server/router.js
@@ -3,9 +3,19 @@ const Router = require('koa-router')
 const router = new Router()
 
 
-router.get('/', (ctx) => {
+
+// Get request --> grab all the moments
+// Post request --> create a new moment
+// (Maybe) Put request --> update or edit a moment
+
+router.get('/moments', (ctx) => {
   console.log('Hello from router')
-  ctx.body = 'Hello from router'
+  ctx.body = 'Get all moments'
+})
+
+router.post('/moments', (ctx) => {
+  console.log('Hello from router')
+  ctx.body = ctx.request.body
 })
 
 
--- Server/server.js
+++ Server/server.js
@@ -1,11 +1,18 @@
 const Koa = require('koa')
 const router = require('./router')
-
+const bodyParser = require('koa-bodyparser')
+const cors = require('@koa/cors')
 const app = new Koa ()
 const PORT = 3051
 
 
+
+
+app.use(cors())
+app.use(bodyParser())
 app.use(router.routes())
+
+
 
 
 
